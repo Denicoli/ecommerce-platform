1ea7d465820f443ce427e5038d95a319e70f13cc
Denicoli <tiagodenicoli@hotmail.com>
2025-06-16 21:24:36 -0300

    feat: add external APIs configuration for Brazilian and European providers

---
+++ backend/src/config/external-apis.ts
@@ -0,0 +1,8 @@
+export const EXTERNAL_APIS = {
+    BRAZILIAN_PROVIDER: {
+        BASE_URL: 'https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider',
+    },
+    EUROPEAN_PROVIDER: {
+        BASE_URL: 'https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider',
+    },
+};
