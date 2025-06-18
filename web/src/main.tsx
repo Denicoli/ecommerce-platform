b55df2279528c939c3cc5d47bde429bfb7eb184e
Denicoli <tiagodenicoli@hotmail.com>
2025-06-18 18:17:25 -0300

    feat: add React Query client and provider to main application

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,13 @@ import React, { StrictMode } from 'react'
 import ReactDOM, { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 )
