07fdb1a13f7cd63c665c477e8ea7632b7015df87
Denicoli <tiagodenicoli@hotmail.com>
2025-06-20 16:14:20 -0300

    feat: configure CORS settings using environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
