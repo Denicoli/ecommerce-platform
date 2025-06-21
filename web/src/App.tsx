a4d6894fd8f72f2ee856bf9990d3c4cebe1ca8b3
Denicoli <tiagodenicoli@hotmail.com>
2025-06-21 18:41:34 -0300

    feat: refactor routing structure to include Register page and conditionally render Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { Login } from './pages/Login'
 import { Checkout } from './pages/Checkout'
@@ -9,10 +9,14 @@ import { useEffect, useState } from 'react'
 import { fetchProducts } from './features/products/api'
 import { CartProvider } from './contexts/CartContext'
 import { CartModal } from './features/cart/components/CartModal'
+import { RequireAuth } from './components/RequireAuth'
+import { Register } from './pages/Register'
 
 function App() {
   const [isCartOpen, setCartOpen] = useState(false)
   const queryClient = useQueryClient()
+  const location = useLocation()
+  const hideHeader = ['/login', '/register'].includes(location.pathname)
 
   useEffect(() => {
     queryClient.prefetchQuery({
@@ -22,24 +26,30 @@ function App() {
   }, [queryClient])
 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <Header onCartClick={() => setCartOpen(true)} />
-          <main className="min-h-screen bg-gray-50">
-            <Routes>
-              <Route path="/" element={<ProductsPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/checkout" element={<Checkout />} />
-            </Routes>
-          </main>
-          <CartModal
-            isOpen={isCartOpen}
-            onClose={() => setCartOpen(false)}
-          ></CartModal>
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <CartProvider>
+        {!hideHeader && <Header onCartClick={() => setCartOpen(true)} />}
+        <main className="min-h-screen bg-gray-50">
+          <Routes>
+            <Route path="/" element={<ProductsPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/checkout"
+              element={
+                <RequireAuth>
+                  <Checkout />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </main>
+        <CartModal
+          isOpen={isCartOpen}
+          onClose={() => setCartOpen(false)}
+        ></CartModal>
+      </CartProvider>
+    </AuthProvider>
   )
 }
 
