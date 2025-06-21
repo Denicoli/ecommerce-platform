bea7781fe70296b4fd4b96250a1ac2a7e348b9b3
Denicoli <tiagodenicoli@hotmail.com>
2025-06-21 10:12:10 -0300

    refactor: enhance register method to check for existing users and improve error handling

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -7,38 +7,46 @@ import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
-    async register(dto: RegisterDto) {
-        const hashedPassword = await bcrypt.hash(dto.password, 10);
-
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                password: hashedPassword,
-                name: dto.name,
-            },
-        });
-
-        return this.createToken(user.id, user.email);
+  async register(dto: RegisterDto): Promise<{ accessToken: string }> {
+    const existingUser = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (existingUser) {
+      throw new ConflictException('Email already in use');
     }
 
-    async login(dto: LoginDto) {
-        const user = await this.prisma.user.findUnique({
-            where: { email: dto.email },
-        });
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-        if (!user || !(await bcrypt.compare(dto.password, user.password))) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+        name: dto.name,
+      },
+    });
 
-        return this.createToken(user.id, user.email);
+    return this.createToken(user.id, user.email);
+  }
+
+  async login(dto: LoginDto): Promise<{ accessToken: string }> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user || !(await bcrypt.compare(dto.password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
     }
 
-    private createToken(userId: string, email: string) {
-        const payload = { sub: userId, email };
-        return {
-            access_token: this.jwt.sign(payload),
-        };
-    }
+    return this.createToken(user.id, user.email);
+  }
+
+  private createToken(userId: string, email: string) {
+    const payload = { sub: userId, email };
+    return {
+      accessToken: this.jwt.sign(payload),
+    };
+  }
 }
